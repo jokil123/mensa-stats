6811707a878e1580006b6f99ac2ce9b5e74767c6
jokil123 <jo.kil.plays@gmail.com>
2024-11-21 18:06:10 +0100

    BLE Scan Test

--- collector/src/main.cpp
+++ collector/src/main.cpp
@@ -1,26 +1,33 @@
+#include <BLEDevice.h>
+#include <BLEScan.h>
 #include <Arduino.h>
 
-// put function declarations here:
-// int myFunction(int, int);
+BLEScan *pBLEScan;
 
 void setup()
 {
-    // put your setup code here, to run once:
-    // int result = myFunction(2, 3);
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
+    BLEDevice::init("");
+    pBLEScan = BLEDevice::getScan();
+    pBLEScan->setActiveScan(true); // Set active scanning for better results
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    // put your main code here, to run repeatedly:
-}
+    Serial.println("Scanning for BLE devices...");
+    BLEScanResults foundDevices = pBLEScan->start(1, false); // Scan for 5 seconds
 
-// put function definitions here:
-// int myFunction(int x, int y)
-// {
-//     // return x + y;
-// }
+    Serial.printf("Found %d devices\n", foundDevices.getCount());
+
+    // for (int i = 0; i < foundDevices.getCount(); i++)
+    // {
+    //     BLEAdvertisedDevice device = foundDevices.getDevice(i);
+    //     Serial.print("Device Address: ");
+    //     Serial.println(device.getAddress().toString().c_str());
+    //     Serial.print("Device Name: ");
+    //     Serial.println(device.getName().c_str());
+    // }
+
+    // delay(10000); // Wait 10 seconds before next scan
+}
