f921adcad3a022aa52fc453defa23b11324e1b19
jokil123 <jo.kil.plays@gmail.com>
2024-11-24 01:11:09 +0100

    maybe fix build

--- api/src/lib/db/mongo.ts
+++ api/src/lib/db/mongo.ts
@@ -1,13 +1,17 @@
 import { MongoClient } from 'mongodb';
-import 'dotenv/config';
+// import 'dotenv/config';
+// import { DB_URL } from '$env/dynamic/private';
 
-if (!process.env.DB_URL) {
-	throw new Error('DB_URL environment variable must be set!');
-}
-const client = new MongoClient(process.env.DB_URL);
+// Default value because otherwise build system breaks lol
+let client: MongoClient = new MongoClient('mongodb://domain.com');
 
 export async function start_mongo() {
 	console.log('Starting mongo...');
+	if (!process.env.DB_URL) {
+		throw new Error('DB_URL environment variable must be set!');
+	}
+
+	client = new MongoClient(process.env.DB_URL);
 	await client.connect();
 	return client;
 }
