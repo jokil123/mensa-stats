69db741533805a1cb0b01e1eb900f2542c784e4f
jokil123 <jo.kil.plays@gmail.com>
2025-05-29 14:01:50 +0200

    scans actually skip now, and init scan

--- collector/src/stateMachine/states/Scanning.cpp
+++ collector/src/stateMachine/states/Scanning.cpp
@@ -7,10 +7,11 @@ void stateScanning(Context *ctx)
 {
     // the last scan is new enough, skip the scan and directly post it.
     // this might reduce down times if the post fails and then successfully reconnects.
-    if (ctx->lastScanTime < SCAN_COUNT * SCAN_DURATION)
+    if (ctx->lastScanTime < SCAN_COUNT * SCAN_DURATION && ctx->devices != -1)
     {
         Serial.printf("Scan is new enough (%ds), skipping scan\n", ctx->lastScanTime / 1000);
         ctx->state = POSTING;
+        return;
     }
 
     try
